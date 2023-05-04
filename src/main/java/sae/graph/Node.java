cfc861724de950598247aa8be94d78ebea1e5b59
tanguydvd85 <116839738+tanguydvd85@users.noreply.github.com>
2023-05-04 09:02:57 +0200

    commit

--- src/main/java/sae/graph/Node.java
+++ src/main/java/sae/graph/Node.java
@@ -1,4 +1,43 @@
 package sae.graph;
 
+import sae.dungeon.Coord;
+
+import java.util.Set;
+
 public class Node {
+    private String name;
+    private Set<Node> neighbors;
+    private Coord coord;
+
+    public Node(String name, Coord coord) {
+        this.name = name;
+        this.coord = coord;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Coord getCoord() {
+        return coord;
+    }
+
+    @Override
+    public boolean equals(Object obj) { //TODO
+        return super.equals(obj);
+    }
+
+    public Set<Node> neighbors() { //TODO
+        Set<Node> var = null;
+        return var;
+    }
+
+    public void addNeigbour(Node node) {
+        //TODO
+    }
+
+    @Override
+    public String toString() { //TODO
+        return super.toString();
+    }
 }
